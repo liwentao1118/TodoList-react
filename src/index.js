a11d8e42a5c74d27863a269d91b9325ac2d0ecca
liwentao1118 <liwentao@liwentaodeMacBook-Pro.local>
2018-07-23 17:29:26 +0800

    第一次提交

--- src/index.js
+++ src/index.js
@@ -1,101 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddItem from './addItem';
-import TodoItem from './todoItem';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TodoItem from './todoItem'
+import AddItem from './addItem'
+import About from './about'
 import './css/index.css'
-import About from './about';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-
-
-const TODO_KEY = "todos";
-class TodoComponent extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        const data = localStorage.getItem(TODO_KEY);
-
-        // 把[]作为默认值
-        this.state = {
-            todos: JSON.parse(data) || []
-        };
-
+import {BrowserRouter as Router, Route ,Link} from 'react-router-dom'
+const KEY = 'todos'
+class ToduComponent extends React.Component{
+    constructor(){
+        super ()
+        let jsonStr  = localStorage.getItem(KEY)
+        ;
+        this.state=({
+            todos:JSON.parse(jsonStr)||[]
+        })
     }
-
-    handleAdd = (val)=>{
-        console.log(val);
-        const todos = this.state.todos;
-        todos.push(val);
+    handleAddItem=(value)=>{
+     let todos = this.state.todos;
+     todos.push(value)
         this.setState({
-            todos
-        });
-
-        localStorage.setItem(TODO_KEY, JSON.stringify(todos));
-    };
-    handleDelete = (index)=>{
-        console.log("删除条目: "+index); // 0 => 1,2
-        let todos = this.state.todos;
-        todos = todos.filter(function (val, i) {
+            todos:todos
+        })
+        localStorage.setItem(KEY,JSON.stringify(todos))
+    }
+    handleDelete=(index)=>{
+        let todos = this.state.todos
+        todos = todos.filter((val,i)=>{
             return index !== i
-        });
-        console.log(todos);
+        })
         this.setState({
-            todos
-        });
-
-        localStorage.setItem(TODO_KEY, JSON.stringify(todos));
-    };
+            todos:todos
+        })
+        localStorage.setItem(KEY,JSON.stringify(todos))
+    }
     render(){
         let subTitle = null
-        let length = this.state.todos.length;
-        if(length % 2 === 0){
-            subTitle = <h3>明日复明日, 明日何其多{length}</h3>;
-        }else {
-            subTitle = <h3>今日事, 今日毕{length}</h3>;
+        let length = this.state.todos.length
+        if (length%2===0){
+            subTitle=<h4>明日复明日,明日何其多  #还剩下{length}条待解决</h4>
+        } else{
+            subTitle=<h4>今日事,今日毕 #还剩下{length}条待解决</h4>
         }
-
-        // 集合的每个元素执行fun函数进行转换, 得到一个新的集合
-        // const list = [1, 2, 3].map(function (val, index) {return val * 2});
-
-        const list = this.state.todos.map((val, index) => {
-            return <TodoItem key={index} index={index} val={val} onDelete={this.handleDelete}/>;
-        });
-        console.log(list);
-
-        if(true){
-            // return null;
-        }
-        return(
+        const list = this.state.todos.map((val,index)=> {
+            return <TodoItem val={val} key={index} index={index} onDelete={this.handleDelete}/>
+        })
+        return (
             <div className='todo-list'>
-                <Link to="/about">关于About</Link>
-
-                { this.props.showTitle && <h2>GTD记事本</h2>}
-
+                <Link to='/about'>关于我的</Link>
+                <h2>代办事项</h2>
                 {subTitle}
-
-                {
-                    length === 0 ?
-                        (<p>恭喜你, 任务全部完成!</p>) :
-                        (<ul>{list}</ul>)
-                }
-
-                <AddItem onInputChange={this.handleAdd}/>
+                {length===0?<p>恭喜你, 事情已经全部完成</p>:<ul>{list}</ul> }
+                <AddItem onInputChange={this.handleAddItem} />
             </div>
         )
     }
 }
-
 class App extends React.Component{
-    render(){
-        return(
+    render (){
+        return (
             <Router>
                 <div>
-                    <Route path="/" exact={true} render={() => <TodoComponent showTitle={true}/>}/>
-                    <Route path="/about" component={About}/>
+                    <Route path='/' exact={true} component={ToduComponent}/>
+                    <Route path='/about' component={About}/>
+
                 </div>
+
             </Router>
         )
     }
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+   <App/>,
+    document.getElementById('root')
+)
